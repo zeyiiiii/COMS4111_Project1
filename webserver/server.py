9591ff07c5afddd504045f81c96c0389eed45649
ZEYI TONG <zt2373@columbia.edu>
2024-11-19 11:27:31 -0500

    Modified home page and built profile page

--- webserver/server.py
+++ webserver/server.py
@@ -18,7 +18,8 @@ Read about it online.
 import os
 from sqlalchemy import *
 from sqlalchemy.pool import NullPool
-from flask import Flask, request, render_template, g, redirect, Response, session, abort, flash, jsonify
+from flask import Flask, request, render_template, g, redirect, Response, session, abort, flash, jsonify, url_for
+from datetime import datetime
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
@@ -113,166 +114,288 @@ def teardown_request(exception):
 # 
 # see for routing: http://flask.pocoo.org/docs/0.10/quickstart/#routing
 # see for decorators: http://simeonfranklin.com/blog/2012/jul/1/python-decorators-in-12-steps/
-#
+# #
+# @app.route('/')
+# def index():
+#   """
+#   request is a special object that Flask provides to access web request information:
+
+#   request.method:   "GET" or "POST"
+#   request.form:     if the browser submitted a form, this contains the data in the form
+#   request.args:     dictionary of URL arguments e.g., {a:1, b:2} for http://localhost?a=1&b=2
+
+#   See its API: http://flask.pocoo.org/docs/0.10/api/#incoming-request-data
+#   """
+
+#   # DEBUG: this is debugging code to see what request looks like
+#   print(request.args)
+
+
+#   #
+#   # example of a database query
+#   #
+#   cursor = g.conn.execute(text("SELECT name FROM test"))
+#   names = []
+#   for result in cursor.mappings():
+#     names.append(result['name'])  # can also be accessed using result[0]
+#   cursor.close()
+
+#   #
+#   # Flask uses Jinja templates, which is an extension to HTML where you can
+#   # pass data to a template and dynamically generate HTML based on the data
+#   # (you can think of it as simple PHP)
+#   # documentation: https://realpython.com/blog/python/primer-on-jinja-templating/
+#   #
+#   # You can see an example template in templates/index.html
+#   #
+#   # context are the variables that are passed to the template.
+#   # for example, "data" key in the context variable defined below will be 
+#   # accessible as a variable in index.html:
+#   #
+#   #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
+#   #     <div>{{data}}</div>
+#   #     
+#   #     # creates a <div> tag for each element in data
+#   #     # will print: 
+#   #     #
+#   #     #   <div>grace hopper</div>
+#   #     #   <div>alan turing</div>
+#   #     #   <div>ada lovelace</div>
+#   #     #
+#   #     {% for n in data %}
+#   #     <div>{{n}}</div>
+#   #     {% endfor %}
+#   #
+#   context = dict(data = names)
+  
+#   posts = []
+#   page = int(request.args.get('page', 1))
+#   posts_per_page = 5
+#   offset = (page - 1) * posts_per_page
+#   current_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+#   try:
+#       query = text(f"""
+#             SELECT * FROM surveys_fill_out
+#             WHERE time >= :current_date
+#             ORDER BY time ASC
+#             LIMIT {posts_per_page} OFFSET {offset}
+#       """)
+#       cursor = g.conn.execute(query, {'current_date': current_date})
+#       for row in cursor.mappings():
+#           posts.append({
+#               'survey_id': row['survey_id'],
+#               'user_id': row['user_id'],
+#               'meal_title': row['meal_title'],
+#               'time': row['time'].strftime('%Y-%m-%d %H:%M'),  # Format the datetime
+#               'state': row['state'],
+#               'city': row['city'],
+#               'address': row['address'],
+#               'menu': row['menu'],
+#               'number_of_people': row['number_of_people'],
+#               'in_return': row['in_return']
+#           })
+#       cursor.close()
+      
+#       # Get the total number of posts for pagination controls
+#       total_query = text("SELECT COUNT(*) AS total FROM surveys_fill_out WHERE time >= :current_date")
+#       total_cursor = g.conn.execute(total_query, {'current_date': current_date})
+#       total_posts = total_cursor.scalar()
+#       total_cursor.close()
+
+#       total_pages = (total_posts + posts_per_page - 1) // posts_per_page
+      
+#   except Exception as e:
+#       print(f"Error fetching data from surveys_fill_out: {e}")
+#       total_pages = 1
+
+#   #
+#   # render_template looks in the templates/ folder for files.
+#   # for example, the below file reads template/index.html
+#   #
+#   if not session.get('logged_in'):
+#     return render_template('login.html')
+#   else:
+#     username = session.get('username', None)
+#     return render_template("index.html", username=username, posts=posts, page=page, total_pages=total_pages)
+
 @app.route('/')
 def index():
-  """
-  request is a special object that Flask provides to access web request information:
-
-  request.method:   "GET" or "POST"
-  request.form:     if the browser submitted a form, this contains the data in the form
-  request.args:     dictionary of URL arguments e.g., {a:1, b:2} for http://localhost?a=1&b=2
-
-  See its API: http://flask.pocoo.org/docs/0.10/api/#incoming-request-data
-  """
-
-  # DEBUG: this is debugging code to see what request looks like
-  print(request.args)
-
-
-  #
-  # example of a database query
-  #
-  cursor = g.conn.execute(text("SELECT name FROM test"))
-  names = []
-  for result in cursor.mappings():
-    names.append(result['name'])  # can also be accessed using result[0]
-  cursor.close()
-
-  #
-  # Flask uses Jinja templates, which is an extension to HTML where you can
-  # pass data to a template and dynamically generate HTML based on the data
-  # (you can think of it as simple PHP)
-  # documentation: https://realpython.com/blog/python/primer-on-jinja-templating/
-  #
-  # You can see an example template in templates/index.html
-  #
-  # context are the variables that are passed to the template.
-  # for example, "data" key in the context variable defined below will be 
-  # accessible as a variable in index.html:
-  #
-  #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
-  #     <div>{{data}}</div>
-  #     
-  #     # creates a <div> tag for each element in data
-  #     # will print: 
-  #     #
-  #     #   <div>grace hopper</div>
-  #     #   <div>alan turing</div>
-  #     #   <div>ada lovelace</div>
-  #     #
-  #     {% for n in data %}
-  #     <div>{{n}}</div>
-  #     {% endfor %}
-  #
-  context = dict(data = names)
-  
-  posts = []
-  page = int(request.args.get('page', 1))
-  posts_per_page = 5
-  offset = (page - 1) * posts_per_page
-  try:
-      query = text(f"SELECT * FROM surveys_fill_out LIMIT {posts_per_page} OFFSET {offset}")
-      cursor = g.conn.execute(query)
-      for row in cursor.mappings():
-          posts.append({
-              'survey_id': row['survey_id'],
-              'user_id': row['user_id'],
-              'meal_title': row['meal_title'],
-              'time': row['time'].strftime('%Y-%m-%d %H:%M'),  # Format the datetime
-              'location': row['location'],
-              'menu': row['menu'],
-              'number_of_people': row['number_of_people'],
-              'in_return': row['in_return']
-          })
-      cursor.close()
-      
-      # Get the total number of posts for pagination controls
-      total_query = text("SELECT COUNT(*) AS total FROM surveys_fill_out")
-      total_cursor = g.conn.execute(total_query)
-      total_posts = total_cursor.scalar()
-      total_cursor.close()
-
-      total_pages = (total_posts + posts_per_page - 1) // posts_per_page
-      
-  except Exception as e:
-      print(f"Error fetching data from surveys_fill_out: {e}")
-
-  #
-  # render_template looks in the templates/ folder for files.
-  # for example, the below file reads template/index.html
-  #
-  if not session.get('logged_in'):
-    return render_template('login.html')
-  else:
-    username = session.get('username', 'Guest')
-    return render_template("index.html", username=username, posts=posts, page=page, total_pages=total_pages)
-
-
-def get_coordinates(address):
-    """
-    Use the Google Geocoding API to convert an address into geographic coordinates (lat, lng).
-    """
-    geocode_url = f"https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={GOOGLE_MAPS_API_KEY}"
-    response = requests.get(geocode_url)
-    data = response.json()
-
-    if data.get('status') == 'OK' and data.get('results'):
-        return data['results'][0]['geometry']['location']  # Returns a dictionary with 'lat' and 'lng'
-    else:
-        raise ValueError("Unable to geocode the address. Please check the input address.")
-
-
-def fetch_posts_from_db():
-    """
-    Fetch posts from the database and return as a list of dictionaries.
-    """
+    username = session.get('username') if session.get('logged_in') else None
+    
     posts = []
+    page = int(request.args.get('page', 1))
+    posts_per_page = 5
+    offset = (page - 1) * posts_per_page
+    current_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
     try:
-        cursor = g.conn.execute(text("SELECT survey_id, user_id, meal_title, time, location, menu, number_of_people, in_return FROM surveys_fill_out"))
+        query = text("""
+            SELECT 
+                s.survey_id,
+                s.user_id,
+                s.meal_title,
+                s.time,
+                s.state,
+                s.city,
+                s.address,
+                s.menu,
+                s.number_of_people,
+                s.in_return,
+                u.username
+            FROM surveys_fill_out AS s
+            JOIN users AS u ON s.user_id = u.user_id
+            WHERE s.time >= :current_date
+            ORDER BY s.time ASC
+            LIMIT :limit OFFSET :offset
+        """)
+        cursor = g.conn.execute(query, {
+            'current_date': current_date,
+            'limit': posts_per_page,
+            'offset': offset
+        })
+
         for row in cursor.mappings():
             posts.append({
                 'survey_id': row['survey_id'],
                 'user_id': row['user_id'],
+                'username': row['username'],
                 'meal_title': row['meal_title'],
-                'time': row['time'].strftime('%Y-%m-%d %H:%M'),  # Format datetime for frontend
-                'location': row['location'],
+                'time': row['time'].strftime('%Y-%m-%d %H:%M'),
+                'state': row['state'],
+                'city': row['city'],
+                'address': row['address'],
                 'menu': row['menu'],
                 'number_of_people': row['number_of_people'],
                 'in_return': row['in_return']
             })
         cursor.close()
+
+        total_query = text("""
+            SELECT COUNT(*) AS total
+            FROM surveys_fill_out AS s
+            WHERE s.time >= :current_date
+        """)
+        total_cursor = g.conn.execute(total_query, {'current_date': current_date})
+        total_posts = total_cursor.scalar()
+        total_cursor.close()
+        total_pages = (total_posts + posts_per_page - 1) // posts_per_page
+
     except Exception as e:
-        print(f"Database Query Error: {e}")
-        raise Exception("Error fetching posts from the database.")
-    return posts
+        print(f"Error fetching posts: {e}")
+        total_pages = 1
 
-
-def filter_nearby_posts(user_coords, posts):
-    """
-    Use the Google Distance Matrix API to filter posts within a 1-hour driving distance.
-    """
-    destinations = "|".join([post['location'] for post in posts])
-    distance_matrix_url = (
-        f"https://maps.googleapis.com/maps/api/distancematrix/json?origins={user_coords['lat']},{user_coords['lng']}"
-        f"&destinations={destinations}&mode=driving&key={GOOGLE_MAPS_API_KEY}"
-    )
-    response = requests.get(distance_matrix_url)
-    data = response.json()
-
-    if data.get('status') == 'OK' and 'rows' in data:
-        filtered_posts = []
-        for i, element in enumerate(data['rows'][0]['elements']):
-            if element['status'] == 'OK' and element['duration']['value'] <= 3600:  # 1 hour = 3600 seconds
-                filtered_posts.append(posts[i])
-        return filtered_posts
-    else:
-        raise ValueError("Error fetching driving distances. Please try again.")
-
+    if not username:
+        return render_template("login.html")
+    else: 
+        return render_template(
+            "index.html", username=username, posts=posts, page=page, total_pages=total_pages
+        )
 
 @app.route('/profile/<username>')
 def profile(username):
-  return render_template("profile.html", username=username)
+    query = text(f"SELECT user_id FROM users WHERE username = :username")
+    uid = g.conn.execute(query, {'username': username}).scalar()
+    
+    reviews, posts, info = [], [], []
+
+    try:
+        # Fetch reviews
+        query = text(f"SELECT * FROM reviews_about WHERE user_id = :uid")
+        with g.conn.execute(query, {'uid': uid}) as cursor:
+            for row in cursor.mappings():
+                reviews.append({
+                    'rate': row['rate'],
+                    'content': row['content']
+                })
+
+        # Collect user info
+        query = text(f"SELECT * FROM users WHERE user_id = :uid")
+        with g.conn.execute(query, {'uid': uid}) as user_info:
+            for row in user_info.mappings():
+                info.append({
+                    'email': row.get('email', 'N/A'),
+                    'tags': row.get('tags', '').split(',') if row.get('tags') else []
+                })
+
+        # Find average rate
+        query = text(f"SELECT AVG(rate) FROM reviews_about WHERE user_id = :uid")
+        avg_rate = g.conn.execute(query, {'uid': uid}).scalar() or 0
+
+        # Find users with a higher average rate
+        query = text("""
+        SELECT COUNT(*) 
+        FROM (
+            SELECT user_id, AVG(rate) AS avg_rate 
+            FROM reviews_about 
+            GROUP BY user_id
+        ) AS user_avg 
+        WHERE user_avg.user_id != :uid AND user_avg.avg_rate > :avg_rate
+        """)
+        higher_rate = g.conn.execute(query, {'uid': uid, 'avg_rate': avg_rate}).scalar() or 0
+
+        # Count total users
+        query = text("SELECT COUNT(*) FROM users")
+        total_users = g.conn.execute(query).scalar() or 0
+
+        # Fetch posts
+        query = text(f"SELECT * FROM surveys_fill_out WHERE user_id = :uid ORDER BY time DESC")
+        with g.conn.execute(query, {'uid': uid}) as cursor:
+            for row in cursor.mappings():
+                posts.append({
+                    'survey_id': row['survey_id'],
+                    'user_id': row['user_id'],
+                    'meal_title': row['meal_title'],
+                    'time': row['time'].strftime('%Y-%m-%d %H:%M'),
+                    'state': row['state'],
+                    'city': row['city'],
+                    'address': row['address'],
+                    'menu': row['menu'],
+                    'number_of_people': row['number_of_people'],
+                    'in_return': row['in_return']
+                })
+    except Exception as e:
+        print(f"Error fetching data for profile: {e}")
+        return render_template("error.html", message="An error occurred while loading the profile.")
+
+    return render_template(
+        "profile.html",
+        username=username,
+        uid=uid,
+        reviews=reviews,
+        posts=posts,
+        rating=avg_rate,
+        rank=higher_rate + 1,
+        total_users=total_users,
+        info=info
+    )
+
+@app.route('/add_review', methods=['POST'])
+def add_review():
+    # Get data from the form
+    user_id = request.form.get('user_id')  # Target user's ID
+    content = request.form.get('content')  # Review content
+    rate = request.form.get('rate')       # Rating
+    print(f"User ID: {user_id}")
+
+    if not user_id or not content or not rate:
+        flash("All fields are required.")
+        return redirect(request.referrer)  # Redirect back to the previous page
+
+    try:
+        # Insert the review into the database
+        query = text("""
+            INSERT INTO reviews_about (user_id, content, rate)
+            VALUES (:user_id, :content, :rate)
+        """)
+        g.conn.execute(query, {'user_id': user_id, 'content': content, 'rate': int(rate)})
+        g.conn.commit()
+        flash("Review submitted successfully!")
+        print("Review inserted successfully")
+    except Exception as e:
+        print(f"Error adding review: {e}")
+        flash("An error occurred while submitting the review.")
+
+    return redirect(url_for('profile', username=request.form.get('username')))
 
 
 #
@@ -306,7 +429,7 @@ def add():
 def do_admin_login():
   POST_USERNAME = str(request.form['username'])
   POST_PASSWORD = str(request.form['password'])
-  if request.form['password'] == '1' and request.form['username'] == 'admin':
+  if request.form['password'] == '1' and request.form['username'] == 'Bob':
     session['logged_in'] = True
     session['username'] = POST_USERNAME
   else:
